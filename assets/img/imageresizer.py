047b8a39cd2cf088cff4c34c7b6aad569ea8caca
gbuchdahl <gbuchdahl@gmail.com>
2017-12-27 00:40:11 -0500

    Automated image resizing!!

---
+++ assets/img/imageresizer.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import sys
+
+location = str(sys.argv[1])
+
+basewidth = 300
+img = Image.open(location)
+wpercent = (basewidth/float(img.size[0]))
+hsize = int((float(img.size[1])*float(wpercent)))
+img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+img.save(location)
